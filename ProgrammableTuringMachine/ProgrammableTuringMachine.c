3f953b7b1c0a72e40f7ed9b7ab3fc60e7b3bb72e
cogrpar <owen.r.welsh@gmail.com>
2022-04-05 16:47:08 -0600

    successfully tested make

--- ProgrammableTuringMachine/ProgrammableTuringMachine.c
+++ ProgrammableTuringMachine/ProgrammableTuringMachine.c
@@ -1,9 +1,16 @@
 #include <lean/lean.h>
 
-int main(int argc, char *argv[]) {
+lean_object* initialize_Lean4TuringMachine(lean_object*);
+
+int main(int argc, char *argv[]) { // TEMP compile with leanc bin/ProgrammableTuringMachine.o bin/Lean4TuringMachine.o bin/AxiomaticSystem.o bin/AxiomaticSystem/dep.o -o output
     printf ("hello world\n");  
 
-    printf ("initializing test lean script...  ");
+    printf ("initializing test lean script...\n");
+    lean_object *res = initialize_Lean4TuringMachine(lean_io_mk_world());
+    if (lean_io_result_is_error(res)) {
+        printf ("an error ocurred, exiting...\n");
+        return EXIT_FAILURE;
+    }
     printf ("done!\n");
 
     return EXIT_SUCCESS;
