11eb59c102e31cd2544dd9185e2a0128cab353c1
cogrpar <owen.r.welsh@gmail.com>
2022-04-05 19:20:40 -0600

    cant use lean_io_result_mk_ok, leads to segmentation fault

--- ProgrammableTuringMachine/ProgrammableTuringMachine.c
+++ ProgrammableTuringMachine/ProgrammableTuringMachine.c
@@ -2,16 +2,8 @@
 
 lean_object* initialize_Lean4TuringMachine(lean_object*);
 
-int main(int argc, char *argv[]) { // TEMP compile with leanc bin/ProgrammableTuringMachine.o bin/Lean4TuringMachine.o bin/AxiomaticSystem.o bin/AxiomaticSystem/dep.o -o output
+int main(int argc, char *argv[]) { // TEMP compile with leanc bin/ProgrammableTuringMachine.o bin/Lean4TuringMachine.o bin/AxiomaticSystem.o bin/AxiomaticSystem/dep.o -o bin/output
     printf ("hello world\n");  
-
-    printf ("initializing test lean script...\n");
-    lean_object *res = initialize_Lean4TuringMachine(lean_io_mk_world());
-    if (lean_io_result_is_error(res)) {
-        printf ("an error ocurred, exiting...\n");
-        return EXIT_FAILURE;
-    }
-    printf ("done!\n");
 
     return EXIT_SUCCESS;
 }
