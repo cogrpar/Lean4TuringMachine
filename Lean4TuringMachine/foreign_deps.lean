dcfb2880012e88c68a6fcfff4787b5b7152efb56
cogrpar <owen.r.welsh@gmail.com>
2022-04-17 13:19:50 -0600

    general transition func c++ function handles arguments

--- Lean4TuringMachine/foreign_deps.lean
+++ Lean4TuringMachine/foreign_deps.lean
@@ -1,9 +1,9 @@
 -- ABI functions
 @[extern "general_transition_function"] constant GeneralTransitionFunction
   /- Q, Γ, and transitions passed from an instance of a TM -/
-  (Q Γ : List String) (transitions : (String × String))
+  (Q : List String) (Γ : List String) (transitions : (String × String))
   /- Machine program as a yaml table stored in a string -/
   (program : String)
   /- transition function of the form '(Q−{q_accept, q_reject}) × Γ → Q × Γ × {−1, 1}' -/
-  (q s : String) : (String × String × String)
+  (q s : String) (Q_len : Float := Q.length.toFloat) (Γ_len : Float := Γ.length.toFloat) : (String × String × String)
 
--- Lean4TuringMachine/foreign_deps_ext.cpp
+++ Lean4TuringMachine/foreign_deps_ext.cpp
@@ -1,21 +1,42 @@
 #include <stdio.h>
-#include <assert.h>
-#include <string>
 #include <lean/lean.h>
 
-extern "C" lean_object * general_transition_function(lean_object *Q, lean_object *Gamma, lean_object *transitions, lean_object *program, lean_object *q, lean_object *s, lean_object /* w */) {
+extern "C" lean_object * general_transition_function(lean_object *Q, lean_object *Gamma, lean_object *transitions, lean_object *table, lean_object *q, lean_object *s, double Q_len, double Gamma_len, lean_object /* w */) {
     // returns (String × (String × String)) of the form Q × Γ × {−1, 1}
 
-    lean_object *ReturnState = lean_mk_string("done");
-    lean_object *ReturnSymbol = lean_mk_string("0");
-    lean_object *ReturnTransition = lean_mk_string("-1");
+    char * Q_array[(int)Q_len];
+    lean_object ** Q_ctor = (lean_to_ctor(Q)->m_objs);
+    for (int i = 0; i < Q_len-1; i++){
+        Q_array[i] = lean_to_string(Q_ctor[0])->m_data;
+        Q_ctor = (lean_to_ctor(Q_ctor[1])->m_objs);
+    }
+    Q_array[(int)Q_len-1] = lean_to_string(Q_ctor[0])->m_data;
 
-    lean_object *res_1 = lean_alloc_ctor(0, 2, 0);
-    lean_object *res_2 = lean_alloc_ctor(0, 2, 0);
+    char * Gamma_array[(int)Gamma_len];
+    lean_object ** Gamma_ctor = (lean_to_ctor(Gamma)->m_objs);
+    for (int i = 0; i < Gamma_len-1; i++){
+        Gamma_array[i] = lean_to_string(Gamma_ctor[0])->m_data;
+        Gamma_ctor = (lean_to_ctor(Gamma_ctor[1])->m_objs);
+    }
+    Gamma_array[(int)Gamma_len-1] = lean_to_string(Gamma_ctor[0])->m_data;
 
-    lean_ctor_set(res_2, 0, ReturnSymbol);
-    lean_ctor_set(res_2, 1, ReturnTransition); // (Γ × {−1, 1})
-    lean_ctor_set(res_1, 0, ReturnState);
+
+    char * transitions_array[2] = {lean_to_string(lean_ctor_get(transitions, 0))->m_data, lean_to_string(lean_ctor_get(transitions, 1))->m_data};
+
+    char * program = lean_to_string(table)->m_data;
+    char * currentState = lean_to_string(q)->m_data;
+    char * currentSymbol = lean_to_string(s)->m_data;
+
+    lean_object * returnState = lean_mk_string(currentState);
+    lean_object * returnSymbol = lean_mk_string(currentSymbol);
+    lean_object * returnTransition = lean_mk_string(Gamma_array[(int)Gamma_len-1]);
+
+    lean_object * res_1 = lean_alloc_ctor(0, 2, 0);
+    lean_object * res_2 = lean_alloc_ctor(0, 2, 0);
+
+    lean_ctor_set(res_2, 0, returnSymbol);
+    lean_ctor_set(res_2, 1, returnTransition); // (Γ × {−1, 1})
+    lean_ctor_set(res_1, 0, returnState);
     lean_ctor_set(res_1, 1, res_2); // (Q × (Γ × {−1, 1}))
 
     return res_1;
