7595b491ffd41f35af4205f7da642f3e448f4b46
cogrpar <owen.r.welsh@gmail.com>
2022-05-14 10:02:42 -0600

    finished table parsing logic and exception handling

--- Lean4TuringMachine/foreign_deps_ext.cpp
+++ Lean4TuringMachine/foreign_deps_ext.cpp
@@ -4,10 +4,19 @@
 #include <lean/lean.h>
 #include <stdexcept>
 
+char* copy (char* input){
+  char* output = new char[strlen(input)+1];
+  for (int i = 0; i < strlen(input); i++){
+    output[i] = input[i];
+  }
+  output[strlen(input)] = '\0';
+  return output;
+}
+
 std::vector<char*> split (char* input, char* delim){
-    char* inputCopy = strdup(input);
+    char* inputCopy = copy(input);
     std::vector<char*> result;
-    result.push_back(std::strtok(inputCopy, delim));
+    result.push_back(strtok(inputCopy, delim));
     char* next = strtok(NULL, delim);
     while (next){
         result.push_back(next);
@@ -16,11 +25,11 @@ std::vector<char*> split (char* input, char* delim){
     return result;
 }
 
-extern "C" lean_object * general_transition_function(lean_object *Q, lean_object *Gamma, lean_object *transitions, lean_object *table, lean_object *q, lean_object *s, double QLen, double GammaLen, lean_object /* w */) {
+extern "C" lean_object * general_transition_function(lean_object * Q, lean_object * Gamma, lean_object * transitions, lean_object * table, lean_object * q, lean_object * s, double QLen, double GammaLen, lean_object /* w */) {
     // returns (String × (String × String)) of the form Q × Γ × {−1, 1}
 
     // convert Q from List String to a char* array
-    char * QArray[(int)QLen];
+    char* QArray[(int)QLen];
     lean_object ** QCtor = (lean_to_ctor(Q)->m_objs);
     for (int i = 0; i < QLen-1; i++){
         QArray[i] = lean_to_string(QCtor[0])->m_data;
@@ -29,7 +38,7 @@ extern "C" lean_object * general_transition_function(lean_object *Q, lean_object
     QArray[(int)QLen-1] = lean_to_string(QCtor[0])->m_data;
 
     // convert Gamma from List String to a char* array
-    char * GammaArray[(int)GammaLen];
+    char* GammaArray[(int)GammaLen];
     lean_object ** GammaCtor = (lean_to_ctor(Gamma)->m_objs);
     for (int i = 0; i < GammaLen-1; i++){
         GammaArray[i] = lean_to_string(GammaCtor[0])->m_data;
@@ -38,12 +47,12 @@ extern "C" lean_object * general_transition_function(lean_object *Q, lean_object
     GammaArray[(int)GammaLen-1] = lean_to_string(GammaCtor[0])->m_data;
 
     // convert transitions from (String × String) to a char* array
-    char * transitionsArray[2] = {lean_to_string(lean_ctor_get(transitions, 0))->m_data, lean_to_string(lean_ctor_get(transitions, 1))->m_data};
+    char* transitionsArray[2] = {lean_to_string(lean_ctor_get(transitions, 0))->m_data, lean_to_string(lean_ctor_get(transitions, 1))->m_data};
 
     // get the program, current state, and current symbol as strings
-    char * program = lean_to_string(table)->m_data;
-    char * currentState = lean_to_string(q)->m_data;
-    char * currentSymbol = lean_to_string(s)->m_data;
+    char* program = lean_to_string(table)->m_data;
+    char* currentState = lean_to_string(q)->m_data;
+    char* currentSymbol = lean_to_string(s)->m_data;
     if ((std::strcmp(currentSymbol, " ")==0)) currentSymbol = "\' \'";
 
     // split program into lines
@@ -54,93 +63,113 @@ extern "C" lean_object * general_transition_function(lean_object *Q, lean_object
         programLines.at(i) = split(programLines.at(i), "#").at(0);
     } 
 
-    // make sure that the blank symbol is in Gamma
-    for (int i = 0; i < programLines.size(); i++){
-        char* blank = strstr(programLines.at(i), "blank");
-        if (blank){
-            std::vector<char*> blankChar = split(blank, "'");
-            if (blankChar.size() < 2){
-                throw "Turing Machine Program Syntax Error";
-            }
-            bool contained = false;
-            for (int j = 0; j < GammaLen && !contained; j++){
-                contained = (std::strcmp(blankChar.at(1), GammaArray[j])==0);
-            }
-            if (!contained){
-                throw "Turing Machine Invalid Blank Symbol";
-            }
-            break;
-        }
-    }
+    lean_object * returnState;
+    lean_object * returnSymbol;
+    lean_object * returnTransition;
 
-    // make sure that the current state and symbol are valid
-    bool valid = false;
-    for (int i = 0; i < QLen; i++){
-        valid = (std::strcmp(QArray[i], currentState)==0);
-        if (valid) break;
-    }
-    if (!valid) throw "Turing Machine Invalid State";
-    valid = false;
-    for (int i = 0; i < GammaLen; i++){
-        valid = (std::strcmp(GammaArray[i], currentSymbol)==0);
-        if (valid) break;
-    }
-    if (!valid) throw "Turing Machine Invalid Symbol";
-
-    // find the transition based on the table
-    int table_line = -1;
-    for (int i = 0; i < programLines.size(); i++){
-        if (strstr(programLines.at(i), "table")){
-            table_line = i;
-            break;
-        }
-    }
-    if (table_line == -1){
-        throw "Turing Machine Program Syntax Error"; // table was not found
-    }
-    char* currentSymbolPossibleOrientations[] = {
-        ' '+currentSymbol, ' '+currentSymbol+' ', ' '+currentSymbol+']', ' '+currentSymbol+',', ' '+currentSymbol+'\'', ' '+currentSymbol+'$',
-        ','+currentSymbol, ','+currentSymbol+' ', ','+currentSymbol+']', ','+currentSymbol+',', ','+currentSymbol+'\'', ','+currentSymbol+'$',
-        '['+currentSymbol, '['+currentSymbol+' ', '['+currentSymbol+']', '['+currentSymbol+',', '['+currentSymbol+'\'', '['+currentSymbol+'$',
-        '\''+currentSymbol, '\''+currentSymbol+' ', '\''+currentSymbol+']', '\''+currentSymbol+',', '\''+currentSymbol+'\'', '\''+currentSymbol+'$'
-    }; // array of possible ways that the current symbol could appear in context in the table
-    for (int i = table_line+1; i < programLines.size(); i++){
-        if (strstr(programLines.at(i), currentState)){
-            try{
-                std::vector<char*> nextLine;
-                bool reachedEnd = false;
-                bool reachedSymbol = false;
-                int steps = 1;
-                while (!reachedEnd){
-                    nextLine = split(programLines.at(i+steps), ":");
-                    std::strcat(nextLine.at(0), "$");
-                    for (int j = 0; j < QLen && !reachedEnd; j++){ // see if the end of this table entry has been reached
-                        if (strstr(nextLine.at(0), QArray[j])) reachedEnd = true;
-                    }
-                    for (int j = 0; j < sizeof(currentSymbolPossibleOrientations) && !reachedSymbol; j++){ // check if this line specifies the action to be taken for the current symbol
-                        if (strstr(nextLine.at(0), currentSymbolPossibleOrientations[j])) reachedSymbol = true;;
-                    }
-                    if (reachedEnd && !reachedSymbol) throw std::out_of_range("end was reached without finding symbol"); // the end was reached without finding the symbol
-                    else if (reachedSymbol){
-                        programLines.at(i+steps) = currentSymbol;
-                        break;
-                    }
-                    steps++;
+    try{
+        // make sure that the blank symbol is in Gamma
+        for (int i = 0; i < programLines.size(); i++){
+            char* blank = strstr(programLines.at(i), "blank");
+            if (blank){
+                std::vector<char*> blankChar = split(blank, "'");
+                if (blankChar.size() < 2){
+                    throw "Turing Machine Program Syntax Error";
                 }
-
+                if ((std::strcmp(blankChar.at(1), " ")==0)) blankChar.at(1) = "\' \'";
+                bool contained = false;
+                for (int j = 0; j < GammaLen && !contained; j++){
+                    if ((std::strcmp(GammaArray[j], " ")==0)) GammaArray[j] = "\' \'";
+                    contained = (std::strcmp(blankChar.at(1), GammaArray[j])==0);
+                }
+                if (!contained){
+                    throw "Turing Machine Invalid Blank Symbol";
+                }
                 break;
             }
-            catch (const std::out_of_range& e) {
-                throw "Turing Machine Program Syntax Error";
+        }
+
+        // make sure that the current state and symbol are valid
+        bool valid = false;
+        for (int i = 0; i < QLen; i++){
+            valid = (std::strcmp(QArray[i], currentState)==0);
+            if (valid) break;
+        }
+        if (!valid) throw "Turing Machine Invalid State";
+        valid = false;
+        for (int i = 0; i < GammaLen; i++){
+            valid = (std::strcmp(GammaArray[i], currentSymbol)==0);
+            if (valid) break;
+        }
+        if (!valid) throw "Turing Machine Invalid Symbol";
+
+        // find the transition based on the table
+        int table_line = -1;
+        for (int i = 0; i < programLines.size(); i++){
+            if (strstr(programLines.at(i), "table")){
+                table_line = i;
+                break;
             }
         }
+        if (table_line == -1){
+            throw "Turing Machine Program Syntax Error"; // table was not found
+        }
+        char symbolPossibleOrientations[20][2] = {
+            {' ', ' '}, {' ', ']'}, {' ', ','}, {' ', '\''}, {' ', '$'},
+            {',', ' '}, {',', ']'}, {',', ','}, {',', '\''}, {',', '$'},
+            {'[', ' '}, {'[', ']'}, {'[', ','}, {'[', '\''}, {'[', '$'},
+            {'\'', ' '}, {'\'', ']'}, {'\'', ','}, {'\'', '\''}, {'\'', '$'}
+        }; // array of possible ways that the current symbol could appear in context in the table
+        for (int i = table_line+1; i < programLines.size(); i++){
+            if (strstr(programLines.at(i), currentState)){
+                try{
+                    std::vector<char*> nextLine;
+                    bool reachedEnd = false;
+                    bool reachedSymbol = false;
+                    int steps = 1;
+                    while (!reachedEnd){
+                        nextLine = split(programLines.at(i+steps), ":");
+                        strcat(nextLine.at(0), "$");
+                        for (int j = 0; j < QLen && !reachedEnd; j++){ // see if the end of this table entry has been reached
+                            if (strstr(nextLine.at(0), QArray[j])) reachedEnd = true;
+                        }
+                        for (int j = 0; j < 20 && !reachedSymbol; j++){ // check if this line specifies the action to be taken for the current symbol
+                            char* possibleOrientation = new char[strlen(currentSymbol)+3];
+                            possibleOrientation[0] = symbolPossibleOrientations[j][0];
+                            for (int k = 0; k < strlen(currentSymbol); k++){
+                                possibleOrientation[k+1] = currentSymbol[k];
+                            }
+                            possibleOrientation[strlen(currentSymbol)+1] = symbolPossibleOrientations[j][1];
+                            possibleOrientation[strlen(currentSymbol)+2] = '\0';
+                            if (strstr(nextLine.at(0), possibleOrientation)) reachedSymbol = true;
+                        }
+                        if (reachedEnd && !reachedSymbol) throw std::out_of_range("end was reached without finding symbol"); // the end was reached without finding the symbol
+                        else if (!reachedEnd && reachedSymbol){
+                            // located table entry for this machine configuration
+                            break;
+                        }
+                        steps++;
+                    }
+
+                    break;
+                }
+                catch (const std::out_of_range& e) {
+                    throw "Turing Machine Program Syntax Error";
+                }
+            }
+        }
+
+        returnState = lean_mk_string(currentState);
+        returnSymbol = lean_mk_string(currentSymbol);
+        returnTransition = lean_mk_string(programLines.at(8));
+
+        // make sure that the return state and symbol are valid
     }
-
-    lean_object * returnState = lean_mk_string(currentState);
-    lean_object * returnSymbol = lean_mk_string(currentSymbol);
-    lean_object * returnTransition = lean_mk_string(programLines.at(8));
-
-    // make sure that the return state and symbol are valid
+    catch (char const* c){ // turing machine error, pass it to returned objects
+        returnState = lean_mk_string(c);
+        returnSymbol = lean_mk_string(c);
+        returnTransition = lean_mk_string(c);
+    }
 
     lean_object * res_1 = lean_alloc_ctor(0, 2, 0);
     lean_object * res_2 = lean_alloc_ctor(0, 2, 0);
